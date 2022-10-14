979e47fd3efc0ef128f1564d3656a850762bb81d
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-10-14 15:24:20 +0100

    endpoint

--- models/endpoints.model.js
+++ models/endpoints.model.js
@@ -1,10 +1,7 @@
 const fs = require("fs/promises");
 
 exports.selectEndpoints = () => {
-  return fs
-    .readFile(`/home/nabeel/northcoders/backend/be-nc-games/endpoints.json`)
-    .then((contents) => {
-      console.log(contents.toString());
-      return contents;
-    });
+  return fs.readFile("./endpoints.json").then((contents) => {
+    return contents;
+  });
 };
