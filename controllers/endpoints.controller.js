89d57d321dd9202ff41faca4f41dbf5d7d5d4223
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-10-14 14:34:21 +0100

    fs readfile used instead

--- controllers/endpoints.controller.js
+++ controllers/endpoints.controller.js
@@ -2,6 +2,7 @@ const { selectEndpoints } = require("../models/endpoints.model");
 
 exports.getEndpoints = (req, res, next) => {
   selectEndpoints().then((endpoints) => {
+    endpoints = JSON.parse(endpoints);
     res.status(200).send({ endpoints });
   });
 };
