3abe6a349bcdfa370e66f5c1b17a5e8d5282e948
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-10-14 11:44:39 +0100

    code written

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,3 +1,9 @@
 const { removeCommentById } = require("../models/comments.model");
 
-exports.deleteCommentById = () => {};
+exports.deleteCommentById = (req, res, next) => {
+  removeCommentById(req.params.comment_id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(next);
+};
--- models/comments.model.js
+++ models/comments.model.js
@@ -1,3 +1,20 @@
 const db = require("../db/connection");
 
-exports.removeCommentById = () => {};
+exports.removeCommentById = (id) => {
+  return db
+    .query(`SELECT * FROM COMMENTS WHERE comment_id = $1`, [id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          message: "Comment_id does not exist",
+        });
+      } else {
+        return db.query(`DELETE FROM comments WHERE comment_id = $1`, [id]);
+      }
+    })
+    .then(() => {
+      console.log("ererer");
+      return Promise.resolve();
+    });
+};
